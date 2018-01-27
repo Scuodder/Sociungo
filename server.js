49cbeb02961fd5df2c87103b8612d24e2830ff54
Scuodder <sudarshan.sudarshanthakur@gmail.com>
2018-01-27 23:24:26 +0530

    server.js added
    Signed-off-by: Scuodder <sudarshan.sudarshanthakur@gmail.com>

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const route = require('./backend/route.js').route
+
+const PORT = process.env.PORT || 2233
+
+
+
+app.engine('html', require('ejs').renderFile );
+app.set('views', __dirname + '/frontend/public')
+app.set('view engine', 'ejs')
+
+
+// requesting login page --------->
+app.use('/', express.static(__dirname + '/frontend/public'))
+
+
+
+// requesting create account page ---------->
+app.use('/', route)
+
+
+
+
+
+
+
+
+
+// user login
+
+
+module.exports = {
+    app , PORT
+}
+
