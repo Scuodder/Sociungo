ba98c13e3746288dff4e4dbd9ce62a8e47cc361e
Scuodder <sudarshan.sudarshanthakur@gmail.com>
2018-02-11 00:21:16 +0530

    cleaning up the code
    Signed-off-by: Scuodder <sudarshan.sudarshanthakur@gmail.com>

--- frontend/public/scripts/profile.js
+++ frontend/public/scripts/profile.js
@@ -7,41 +7,6 @@ $(function(){
     let nHome = $('#nHome');
     let nSettings = $('#nSettings');
 
-//    // random number generator
-//    function random (min, max) {
-            
-//     let mi = Math.ceil(min);
-//     let ma = Math.floor(max);
-//     return Math.floor(Math.random() * (ma - mi)) + mi;
-// }
-
-//     function processNews(data) {
-//         let noOfResults = data.totalResults,
-//         i = random(0, noOfResults),
-//         title = data.articles[i].title,
-//         description  =  data.articles[i].description,
-//         source = data.articles[i].source.name,
-//         author = data.articles[i].author,
-//         site = data.articles[i].url,
-//         image = data.articles[i].urlToImage;
-
-
-//         if(author===null) author = 'unknown';
-//         if(description===null) description = "";
-        
-//         contain.empty();
-//         contain.append($(`
-        
-//         <img src=${image} class = "rounded p-2 " width="370px" alt="" ></img>
-//         <h4 class="p-2">${title}</h4>
-//         <p class="lead p-2" >${description}</p> 
-//         <span class="p-2"> #Source-${source}</span> <br>
-//         <span class="p-2"> #Author-${author}</span> <br>
-//         <a href=${site} class="p-2">Visit</a> 
-        
-//         `))
-//     }
-
     // get AJAX request to fetch news data when page is loaded .....
     
     $.get("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=19eb4ff1d1654a6cac5106b3087408d3", function(data) {
